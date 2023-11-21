a5ff1ed0a12d09f2c64445a8f46d8a0c79863d46
NRDsstuff <nrdfoss@gmail.com>
2023-11-21 10:15:46 +0100

    started work on zeromod

---
+++ src/other/zeromod/typing.ts
@@ -0,0 +1,16 @@
+export interface profile{
+
+    hp: number
+    strikes: strike[]
+    ids: number[] // no ban evasion :3
+    quarantined: boolean
+}
+
+export interface strike{
+
+    case: number
+    explanation: string
+    proof: string
+    damage: number
+    timestamp: number // epoch time thingy
+}
--- src/plugins/zeroMod.ts
+++ src/plugins/zeroMod.ts
@@ -8,6 +8,9 @@ export = {
     version: "1.0.0",
     run: (tools: PluginTools) =>{
         
+        // make it check for all members in zero negativum and assign them a new profile if there isn't any assigned to them
+        // mods should be able to assign the user to their profile
+        // i have no clue how to do this doe and i'm at school so ehh
         log("ready", 4, "zeroMod", true)
     }
 }
