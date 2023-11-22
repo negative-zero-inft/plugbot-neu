c1877278d2a1e1e482b8a8e37f6757913a6d5d3e
NRDsstuff <nrdfoss@gmail.com>
2023-11-22 09:39:30 +0100

    some work on pbpm

--- src/commands/pbpm.ts
+++ src/commands/pbpm.ts
@@ -1,13 +1,25 @@
 import { CmdTools } from "../other/typing"
+import * as repos from "../configs/pbpm/repos.json"
+import { log } from "../other/utils"
 
 module.exports = {
     name: "pbpm",
     developers: ["nrd"],
     version: "0.0.1",
     desc: "the plugbot package manager",
-    usage: "pbpm [i/u/r] [package name]",
+    usage: "pbpm [a/i/u/r] [package name]",
     run: (tools: CmdTools) =>{
     
-        // TODO: actually work on this
+        if(!tools.input.args[1]) return log("please input what action you want to perform", 1, "pbpm", true)
+        switch(tools.input.args[1].toLowerCase()){
+            
+            case "add":
+                // adds a repo
+                break
+
+            case "a":
+                // adds a repo
+                break
+        }
     }
 }
