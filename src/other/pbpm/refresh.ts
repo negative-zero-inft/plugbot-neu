464c150f3dd5266e9bf88eed665f50d3a4bb5252
NRDsstuff <nrdfoss@gmail.com>
2023-12-30 10:36:29 +0100

    imported a bunch of stuff :3

--- src/other/pbpm/refresh.ts
+++ src/other/pbpm/refresh.ts
@@ -1,4 +1,55 @@
-export default function a() {
+import { log } from "../utils";
+import { writeFile, rmSync } from "fs";
+import { CmdTools, pbpmRepo } from "../typing";
+import { JSONrequire } from "../utils";
+import { SimpleGit, simpleGit, SimpleGitOptions } from "simple-git";
 
+export default function a(tools: CmdTools, repos: pbpmRepo[]) {
 
+    const options: Partial<SimpleGitOptions> = {
+        baseDir: "./temp/",
+        binary: "git",
+        maxConcurrentProcesses: 6,
+        trimmed: false,
+    };
+    const git: SimpleGit = simpleGit(options);
+    git.clone(tools.input.args[2], "pbpm", {}, (err) => {
+
+        if (err) {
+
+            log(`failed to fetch the repo at ${tools.input.args[2]}:`, "pbpm", {
+                saveFile: true,
+                username: tools.account.name,
+                level: 2
+            });
+            console.error(err);
+            return;
+        }
+        const repo: pbpmRepo = JSONrequire("../../temp/pbpm/repo.json");
+        if (repos.find(x => x.id === repo.id)) return log(`the repo with the id ${repo.id} has been installed already.`, "pbpm", {
+            username: tools.account.name,
+            level: 1
+        });
+        tools.cmdTools.rl.question(`you're about to add ${repo.name}. continue? [Y/N] `, (a) => {
+
+            if (["y", "yes"].includes(a.toLowerCase())) return log("cancelling the addition of the repo", "pbpm", { username: tools.account.name, });
+        });
+        repos.push(repo);
+        writeFile("./userSpace/repos.json", JSON.stringify(repos), (err) => {
+
+            if (err) {
+
+                log("couldn't save new repo:", "pbpm", {
+                    username: tools.account.name,
+                    level: 2
+                });
+                console.error(err);
+            }
+            tools.cmdTools.mem.set("pbpm", {
+
+                lastAction: "add"
+            });
+            rmSync("./temp/pbpm", { recursive: true });
+        });
+    });
 }
